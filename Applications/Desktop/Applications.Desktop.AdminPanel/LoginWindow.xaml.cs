f58e7d24af5cbfae99b180755b0a6c473c1844b6
IrinaKu14 <irina.kutlatova@gmail.com>
2024-11-24 21:06:57 +0400

    addcode

--- Applications/Desktop/Applications.Desktop.AdminPanel/LoginWindow.xaml.cs
+++ Applications/Desktop/Applications.Desktop.AdminPanel/LoginWindow.xaml.cs
@@ -23,23 +23,43 @@ namespace ProjectC.Applications.Desktop.AdminPanel
         {
             InitializeComponent();
 
+            //< !--< Button Grid.Row = "3" Grid.Column = "0"  Margin = "10" > ЗАБЫЛИ ПАРОЛЬ ?</ Button >
+            //< Button Grid.Row = "3" Grid.Column = "1"  Margin = "10" Width = "250" > РЕГИСТРАЦИЯ </ Button > -->
+
+           var buttonOne = new Button()
+            {
+                Content = "ЗАБЫЛИ ПАРОЛЬ ?",
+                Margin = new Thickness(10),
+            };
+            var buttonTwo = new Button()
+            {
+                Content = "РЕГИСТРАЦИЯ",
+                Margin = new Thickness(10),
+
+            };
+            mainGrid.Children.Add(buttonOne);
+            Grid.SetColumn(buttonOne, 0);
+            Grid.SetRow(buttonOne, 3);
+            mainGrid.Children.Add(buttonTwo);
+            Grid.SetColumn(buttonTwo, 1);
+            Grid.SetRow(buttonTwo, 3);
         }
 
-        private void ButtonClick(object sender, RoutedEventArgs e)
-        {
+        //private void ButtonClick(object sender, RoutedEventArgs e)
+        //{
             
-            var result = MessageBox.Show("Привет, мир!", "Приветственное окно", MessageBoxButton.YesNo, MessageBoxImage.Error);
-            if (result == MessageBoxResult.Yes)
-            {
-                MessageBox.Show("Да", startButton.Content.ToString());
-            }
-            else
-            {
-                MessageBox.Show("Нет", startButton.Content.ToString());
-            }
+        //    var result = MessageBox.Show("Привет, мир!", "Приветственное окно", MessageBoxButton.YesNo, MessageBoxImage.Error);
+        //    if (result == MessageBoxResult.Yes)
+        //    {
+        //        MessageBox.Show("Да", startButton.Content.ToString());
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Нет", startButton.Content.ToString());
+        //    }
 
             //test commnt
 
-        }
+        //}
     }
 }
