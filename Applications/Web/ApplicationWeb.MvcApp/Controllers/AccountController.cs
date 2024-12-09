dd39cdffd22781d1dffb3067524d00b25009545f
IrinaKu14 <irina.kutlatova@gmail.com>
2024-12-09 20:56:41 +0400

    add Account-Controller

---
+++ Applications/Web/ApplicationWeb.MvcApp/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationWeb.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationWeb.MvcApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("Login", new LoginViewModel());
+        }
+        public IActionResult Account()
+        {
+            return View("Account");
+        }
+        public IActionResult Logout()
+        {
+            return View("Login");
+        }
+    }
+}
