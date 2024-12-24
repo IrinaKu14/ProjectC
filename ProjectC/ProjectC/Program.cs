039ec13f03439b573f0e1c5b6f238e2783aad15d
IrinaKu14 <irina.kutlatova@gmail.com>
2024-12-24 21:27:15 +0400

    Add Interface

--- ProjectC/ProjectC/Program.cs
+++ ProjectC/ProjectC/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ProjectC
+﻿namespace ProjectC;
+
+//[Table("main_messages")]
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
+
+        var storage = new DatabaseStorage();
+        storage.SetValue("SERVER_ADDRESS", "localhost:8080");
+        Console.WriteLine(storage.GetValue("SERVER_ADDRESS"));
+        SellerPanel(storage);
+        UserPanel(storage);
+        AdminPanel(storage);
     }
+
+    public static void UserPanel(DatabaseStorage storage)
+    {
+        Console.WriteLine(storage.GetValue("PRODUCT_ID"));
+
+    }
+    public static void AdminPanel(DatabaseStorage storage)
+    {
+        Console.WriteLine(storage.GetValue("SERVER_ADDRESS"));
+    }
+    public static void SellerPanel(DatabaseStorage storage)
+    {
+        storage.SetValue("PRODUCT_ID", "123456");
+        Console.WriteLine(storage.GetValue("PRODUCT_ID"));
+    }
+
 }
